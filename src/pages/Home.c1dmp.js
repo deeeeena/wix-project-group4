642baaf407aaf74a301478213bb32049799e9da4
deeeeena <144300929+deeeeena@users.noreply.github.com>
2024-09-26 09:29:55 +0800

    Update Home.c1dmp.js

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -3,7 +3,7 @@
 
 $w.onReady(function () {
     // Write your JavaScript here
-
+    // update image in gallery
     // To select an element by ID use: $w('#elementID')
 
     // Click 'Preview' to run your code
