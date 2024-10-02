2068fd6b926e91bd021c874678b9a0918462d655
monchertulipe <154590120+monchertulipe@users.noreply.github.com>
2024-10-02 13:12:13 +0800

    Update Donation details.vp4o8.js

--- src/pages/Donation details.vp4o8.js
+++ src/pages/Donation details.vp4o8.js
@@ -3,6 +3,10 @@
 
 $w.onReady(function () {
     // Write your JavaScript here
+    // created paraphrase to convince the viewer to donate
+    // explain where does the donation will go to
+    // created donate button
+    
 
     // To select an element by ID use: $w('#elementID')
 
